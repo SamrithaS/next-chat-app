251e5f67e3dcdf1d9aedcedcddc334238d2a45af
SamrithaS <samritha@timeless.co>
2022-08-17 17:30:08 +0530

    add moduleResolution to tsconfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import { useEffect } from "react";
 import { supabase } from "../helpers/supabase";
 
 function MyApp({ Component, pageProps }: AppProps) {
- 
   return <Component {...pageProps} />;
 }
 
